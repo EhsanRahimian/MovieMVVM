77da821ea2bf87788b0682f8497b4f1602551195
Ehsan <ehrath1@gmail.com>
2020-07-13 10:49:00 -0700

    Designing ui and creating network

---
+++ app/src/main/java/com/nicootech/movieappmvvm/single_movie_details/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.nicootech.movieappmvvm.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nicootech.movieappmvvm.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
